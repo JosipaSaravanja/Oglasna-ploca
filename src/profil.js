9fea405df33be49b17ae6c21a9f33f73e434ec67
JosipaSaravanja <77886345+JosipaSaravanja@users.noreply.github.com>
2021-03-27 18:25:10 +0100

    Delete src directory

--- src/profil.js
+++
@@ -1,7 +0,0 @@
-
-import Component from "./baseComponent";
-import Controler from "./modelAndControler";
-let controler = new Controler()
-import IzgledProfila from "./components/izgledProfila";
-alert("HELLO")
-document.getElementById("srednjiStupac").appendChild(new IzgledProfila().rootElement)
